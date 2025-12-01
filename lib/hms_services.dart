bd84abf3f711fad888c6c60d4c4f10d9c4536408
n1k5tas <unitylicenziuv@gmail.com>
2025-12-01 11:06:59 +0500

    Remove Android and iOS platform implementations, including Gradle files, plugin classes, and associated test files for HMS services, streamlining the project structure.

--- lib/hms_services.dart
+++ lib/hms_services.dart
@@ -1,8 +1,1 @@
 
-import 'hms_services_platform_interface.dart';
-
-class HmsServices {
-  Future<String?> getPlatformVersion() {
-    return HmsServicesPlatform.instance.getPlatformVersion();
-  }
-}
